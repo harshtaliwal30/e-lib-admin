602d8afb484d5d7625f4845c06c86442faa92bd3
harshtaliwal30 <harshtaliwal@gmail.com>
2021-09-06 23:55:00 +0530

    UI Enhancement

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:e_lib_admin/Utils/app_routes.dart';
+import 'package:e_lib_admin/Utils/utils.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -15,11 +16,19 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarColor: Utils.white,
+        statusBarIconBrightness: Brightness.dark,
+      ),
+    );
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        accentColor: Colors.white70,
+        fontFamily: "OpenSans",
       ),
       getPages: AppRoutes.routes(),
       initialRoute: AppRoutes.splashScreenRoute,
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:e_lib_admin/Utils/app_ui_constant.dart';
+import 'package:e_lib_admin/Utils/utils.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,8 +9,115 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Home Screen"),
+      backgroundColor: Utils.white,
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Utils.white,
+        title: Text(
+          "Library Name",
+          style: TextStyle(
+            color: Utils.black,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Card(
+              shadowColor: Utils.black,
+              // elevation: 0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10),
+                ),
+                side: BorderSide(
+                  color: Utils.black,
+                  width: 0.5,
+                ),
+              ),
+              margin: EdgeInsets.only(
+                left: AppUIConst.safeBlockHorizontal * 2,
+                right: AppUIConst.safeBlockHorizontal * 2,
+                bottom: AppUIConst.safeBlockHorizontal * 2,
+              ),
+              child: Container(
+                padding: EdgeInsets.all(
+                  AppUIConst.safeBlockHorizontal * 2,
+                ),
+                height: AppUIConst.screenHeight / 6,
+                child: Row(
+                  children: [
+                    Container(
+                      height: AppUIConst.screenHeight / 6,
+                      width: AppUIConst.screenWidth / 4,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                        child: CachedNetworkImage(
+                          fit: BoxFit.fill,
+                          imageUrl:
+                              "https://firebasestorage.googleapis.com/v0/b/e-lib-e53d6.appspot.com/o/28828394_950086555154619_9137006216337597187_o.jpg?alt=media&token=dd2c5145-1e19-43d6-ace7-6bede86f7a7d",
+                          errorWidget: (context, url, error) => Icon(Icons.error),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(
+                              left: AppUIConst.safeBlockHorizontal * 3,
+                              top: AppUIConst.safeBlockHorizontal * 4,
+                            ),
+                            child: Text(
+                              "Cracking the coding interview",
+                              overflow: TextOverflow.ellipsis,
+                              softWrap: true,
+                              style: TextStyle(
+                                color: Utils.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: AppUIConst.safeBlockHorizontal * 4.5,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(
+                              left: AppUIConst.safeBlockHorizontal * 3,
+                              top: AppUIConst.safeBlockHorizontal * 1,
+                            ),
+                            child: Text(
+                              "Quantity",
+                              style: TextStyle(
+                                color: Utils.black,
+                                fontSize: AppUIConst.safeBlockHorizontal * 4,
+                              ),
+                            ),
+                          ),
+                          Spacer(),
+                          Padding(
+                            padding: EdgeInsets.only(
+                              left: AppUIConst.safeBlockHorizontal * 3,
+                              bottom: AppUIConst.safeBlockHorizontal * 2,
+                            ),
+                            child: Text(
+                              "Price",
+                              style: TextStyle(
+                                color: Utils.black,
+                                fontSize: AppUIConst.safeBlockHorizontal * 4,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
